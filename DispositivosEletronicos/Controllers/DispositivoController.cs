6b43520c2569694ae500f48d7c25cebf122de3f6
1Wenderr <wender.penha@aluno.senai.br>
2024-11-13 07:26:10 -0300

    Adicionar arquivos de projeto.

---
+++ DispositivosEletronicos/Controllers/DispositivoController.cs
@@ -0,0 +1,20 @@
+﻿using DispositivosEletronicos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace DispositivosEletronicos.Controllers
+{
+    public class DispositivoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<DispositivoEletronico> dispositivos = new List<DispositivoEletronico>
+            {
+                new Computador {Marca = "Dell", Modelo = "Inspirion", AnoDeFabricacao = 2021, EhLaptop = true, MemoriaRam = 16, SistemaOperacional = "Windows 11"},
+                new Smartphone {Marca = "Samsung", Modelo = "Galaxy S24", AnoDeFabricacao = 2024, Tem5G = true}
+            }; 
+
+            return View(dispositivos);
+        }
+    }
+}
